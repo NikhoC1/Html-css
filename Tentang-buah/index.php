aba5e1dcdcc24d720cbfbb0ea22c16eb95b682e3
NikhoC1 <110102550+NikhoC1@users.noreply.github.com>
2022-11-06 16:46:00 +0700

    Add files via upload

---
+++ Tentang-buah/index.php
@@ -0,0 +1,58 @@
+<?php?
+
+
+
+>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tentangbuah.com - website tentang buah !</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+    <div class="content">
+        <header>
+            <img src="../Image/buah.png" alt="">
+            <h1>tentangbuah.com</h1>
+    <nav>
+    <div>
+</div>
+    <ul>
+        <li> <a href="index.html">Home</a></li>
+        <li><a href="#">Buah</a>
+            <ul>
+                <li><a href="pages/apel.html">Apel</a></li>
+                <li><a href="pages/jeruk.html">Jeruk</a></li>
+                <li><a href="pages/durian.html">Durian</a></li>
+                <li><a href="pages/pisang.html">Pisang</a></li>
+            </ul>
+        </li>
+        <li> <a href="pages/kontak.htmla">kontak</a></li>
+    </ul>
+</nav>
+</header>
+<section>
+    <h2>Definisi</h2>
+<p>
+    <strong>Buah</strong> adalah organ pada tumbuhan berbunga yang merupakan perkembangan lanjutan dari bakal buah (ovarium). Buah biasanya membungkus dan melindungi biji. Aneka rupa dan bentuk buah tidak terlepas kaitannya dengan fungsi utama buah, yakni sebagai pemencar biji tumbuhan. 
+    <br /><br>
+Pengertian buah dalam lingkup pertanian (<em>hortikultura</em>) atau pangan adalah lebih luas daripada pengertian buah di atas dan biasanya disebut sebagai <strong>buah-buahan</strong>.Buah dalam pengertian ini tidak terbatas yang terbentuk dari bakal buah, melainkan dapat pula berasal dari perkembangan organ yang lain. Karena itu, untuk membedakannya, buah yang sesuai menurut pengertian botani biasa disebut <strong>buah sejati</strong>.<br /><br />
+Buah sering kali memiliki nilai ekonomi sebagai bahan pangan maupun bahan baku industri karena di dalamnya disimpan berbagai macam produk metabolisme tumbuhan, mulai dari karbohidrat, protein, lemak, vitamin, mineral, alkaloid, hingga terpena dan terpenoid. Ilmu yang mempelajari segala hal tentang buah dinamakan <strong>pomologi</strong>.
+</p>
+</section>
+<footer>
+    <p> <i class="fa fa copyright" aria-hidden="true"> Created by Nikho</i></p>
+</footer>
+    </div>  
+    </div>
+</body>
+</div>
+</html>
